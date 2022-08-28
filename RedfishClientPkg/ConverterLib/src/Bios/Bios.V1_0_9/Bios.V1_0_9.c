e2811758c9526f06e788e431d5d60ca9b1ad6828
Abner Chang <abner.chang@amd.com>
2022-08-28 19:19:18 +0800

    edk2-staging/RedfishClientPkg: Update Redfish converter lib
    Temporary modified on the auto-generated lib to support
    Redfish BIOS attributes. RedfishScemaToCStructure python
    script needs to be updated for fixing this issue.
    
    Signed-off-by: Abner Chang <abner.chang@amd.com>
    Cc: Yang Atom <Atom.Yang@amd.com>
    Cc: Nick Ramirez <nramirez@nvidia.com>
    Cc: Nickle Wang <nickle@csie.io>
    Cc: Igor Kulchytskyy <igork@ami.com>
    Reviewed-by: Nickle Wang <nickle@csie.io>

--- RedfishClientPkg/ConverterLib/src/Bios/Bios.V1_0_9/Bios.V1_0_9.c
+++ RedfishClientPkg/ConverterLib/src/Bios/Bios.V1_0_9/Bios.V1_0_9.c
@@ -367,17 +367,17 @@ Error:;
 }
 static RedfishCS_status CS_To_JSON_Attributes(json_t *CsJson, char *Key, RedfishBios_V1_0_9_Attributes_CS *CSPtr)
 {
-  json_t *CsParentJson;
+  //json_t *CsParentJson;
 
   if (CSPtr == NULL) {
     return RedfishCS_status_success;
   }
 
-  CsParentJson = CsJson;
-  CsJson = json_object();
-  if (CsJson == NULL) {
-    return RedfishCS_status_unsupported;
-  }
+  //CsParentJson = CsJson;
+  //CsJson = json_object();
+  //if (CsJson == NULL) {
+  //  return RedfishCS_status_unsupported;
+  //}
 
   // Check if this is RedfishCS_Type_CS_EmptyProp.
   CsEmptyPropLinkToJson(CsJson, Key, &CSPtr->Prop);
